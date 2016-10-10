595eccf23ca14e29908923b84bc2b98ed67121ba
Alajuwon Thomas <spooda7@yahoo.com>
2016-10-10 15:57:32 -0400

    first actual commit

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-<!doctype html>
-<html>
-<head>
-<title>The Function esty Assignment</title>
-<body>
+console.log("loaded")
+
+//#1
+
+var avgPrice =0;
+for (var i = 0;i<items.length; i++){
+	avgPrice=avgPrice+items[i].price
+}
+
+items.length
+var total=avgPrice/items.length
+console.log(total)
+
+
+
+//#2
+var list = items.filter(function(element){
+	if (element.price > 14 && element.price <18){
+		return true;
+	} else { return false;}
+})
+
+console.log(list);
 
 
 
@@ -13,11 +31,3 @@
 
 
 
-
-
-
-
-
-</body>
-</head>
-</html>
