bd185ff8cf5ef9ab428ed487f52b8b4826a9d791
Alajuwon Thomas <spooda7@yahoo.com>
2016-10-10 19:02:15 -0400

    modifications

--- main.js
+++ main.js
@@ -2,30 +2,54 @@ console.log("loaded")
 
 //#1
 
-var avgPrice =0;
-for (var i = 0;i<items.length; i++){
+	var avgPrice =0;
+	for (var i = 0;i<items.length; i++){
 	avgPrice=avgPrice+items[i].price
 }
 
-items.length
-var total=avgPrice/items.length
-console.log(total)
+	items.length
+	var total=avgPrice/items.length
+	
 
 
 
 //#2
-var list = items.filter(function(element){
+	var list = items.filter(function(element){
 	if (element.price > 14 && element.price <18){
 		return true;
 	} else { return false;}
 })
 
-console.log(list);
+	
+
+
+
+//#3
+
+	var code = items.filter(function(element){
+	return (element.currency_code === "GBP")
+
+})
 
 
 
 
+//#4 
+	var tree = items.filter(function(x){
+		return (x.materials.includes("wood"))
+	})
 
+
+//#5
+	var materials = items.filter(function(y){
+		return(y.materials.length > 8)
+	})
+
+	
+//#6
+	var sellers = items.filter(function(z){
+		return(z.who_made==="i_did")
+	})
 
 
 
